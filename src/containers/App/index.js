c70526b96a021243c8a14c01d0dda2083624273b
Dan Cadden <Shikkic@gmail.com>
2017-11-02 18:40:23 -0400

    pruning

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './style.css';
 
 class App extends Component {
@@ -7,11 +6,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p>
+          Hello World
         </p>
       </div>
     );
